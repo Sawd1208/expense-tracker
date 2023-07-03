9b2e737a98b0f33e3b5664ef303be91936715c69
sawd1208 <sawdchien@gmail.com>
2023-07-04 01:38:52 +0800

    add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -11,7 +11,8 @@ router.get('/login', (req, res) => {
 // 加入 middleware，驗證 request 登入狀態
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/register', (req, res) => {
@@ -21,13 +22,30 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
   // 取得註冊者的表單參數
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   // 檢查註冊者是否有註冊過
   User.findOne({ email })
     .then((user) => {
       // 如果已經註冊過，返回註冊頁面
       if (user) {
-        console.log('User already exist.')
+        errors.push({ message: '這個 Email 已經註冊過了。' })
         res.render('register', {
+          errors,
           name,
           email,
           password,
@@ -49,6 +67,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '成功登出')
   res.redirect('/users/login')
 })
 
