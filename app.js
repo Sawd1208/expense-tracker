73cb1f0a67e94c03e49ab42ae2aae9fb08bf6567
sawd1208 <sawdchien@gmail.com>
2023-07-03 23:04:50 +0800

    add user login feature via passport

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
-
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
+const routes = require('./routes')
+
+const usePassport = require('./config/passport')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
 
-const routes = require('./routes')
-const { trusted } = require('mongoose')
-
-require('./config/mongoose')
+// const { trusted } = require('mongoose')
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -26,9 +27,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+usePassport(app)
+
 app.use(routes)
-
-
 
 app.listen(port, () => {
   console.log(`The App is running on http://localhost:${port}`)
