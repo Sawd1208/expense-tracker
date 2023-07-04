9a993f6b4ddaaf5cb73ca84f4ed61a6cbd66f28e
sawd1208 <sawdchien@gmail.com>
2023-07-05 00:32:36 +0800

    add facebook login

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 
+if(process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
-
-// const { trusted } = require('mongoose')
+const port = process.env.PORT
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
