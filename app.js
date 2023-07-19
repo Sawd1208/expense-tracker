177eee994823c837d24cd5c85f61462c865779b5
sawd1208 <sawdchien@gmail.com>
2023-07-19 16:58:39 +0800

    modify PORT

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -42,6 +42,6 @@ app.use((req, res, next) => {
 
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`The App is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`The App is running on http://localhost:${PORT}`)
 })
